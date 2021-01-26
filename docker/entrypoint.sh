d722156152b5c962755d617837edaf43101d60ae
clamp <15528374+DJ-clamp@users.noreply.github.com>
2021-01-26 11:46:56 +0800

    add entrypoint.sh and rename Dockerfile to Dockerfile_unicom

---
+++ docker/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+crontab -r
+if [ ${enable_52pojie} ];then
+  echo "10 13 * * *       node ${ASM_DIR}/scripts/index.js 52pojie --htVD_2132_auth=${htVD_2132_auth} --htVD_2132_saltkey=${htVD_2132_saltkey}" >> /etc/crontabs/root
+fi
+
+if [ ${enable_bilibili} ];then
+  echo "*/30 7-22 * * *       node ${ASM_DIR}/scripts/index.js bilibili --cookies ${cookies} --username ${username} --password ${password} ${othercfg}" >> /etc/crontabs/root
+fi
+
+if [ ${enable_iqiyi} ];then
+  echo "*/30 7-22 * * *       node ${ASM_DIR}/scripts/index.js iqiyi --P00001 ${P00001} --P00PRU ${P00PRU} --QC005 ${QC005}  --dfp ${dfp}" >> /etc/crontabs/root
+fi
+
+if [ ${enable_unicom} ];then
+  echo "*/30 7-22 * * *       node ${ASM_DIR}/scripts/index.js unicom --cookies ${cookies} --user ${user} --password ${password} --appid ${appid}" >> /etc/crontabs/root
+fi
+
+if [ ${enable_10086} ];then
+  echo "10 13 * * *       node ${ASM_DIR}/scripts/index.js 10086 --cookies ${cookies}" >> /etc/crontabs/root
+fi
+
+/usr/sbin/crond -c /etc/crontabs -f
+
+tail -f /var/log/cron.log
