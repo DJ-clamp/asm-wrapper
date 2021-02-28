302353b36e87296ad3ecf5a994a3bd2c945a7043
clamp <15528374+DJ-clamp@users.noreply.github.com>
2021-02-28 12:43:04 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir -p /root/.ssh  && echo -e ${KEY} > /root/.ssh/id_rsa  && chmod 600 /root/.ssh/id_rsa  && ssh-keyscan github.com > /root/.ssh/known_hosts
-
 echo "设定远程仓库地址..."
+mkdir -p /root/.ssh  && echo -e ${KEY} > /root/.ssh/id_rsa  && chmod 600 /root/.ssh/id_rsa  && ssh-keyscan github.com > /root/.ssh/known_hosts
 cd ${ASM_DIR}/scripts
 git clone --no-checkout -b ${ASM_SCRIPTS_BRANCH} ${REPO_URL} ${ASM_DIR}/tmp
 mv ${ASM_DIR}/tmp/.git ${ASM_DIR}/scripts
